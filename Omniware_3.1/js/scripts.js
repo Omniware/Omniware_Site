4a6a97e58364eefd5d121839d25d4a78e3171a7a
Shivakrishnat <shivakrishnat095@gmail.com>
2015-01-05 17:16:23 +0530

    Final_Omniware_3.1

---
+++ Omniware_3.1/js/scripts.js
@@ -0,0 +1,463 @@
+/*animated row*/
+/*=====1======*/
+(function($) {
+	
+	var methods = {
+			init 	: function( options ) {
+				
+				if( this.length ) {
+					
+					var settings = {
+						// configuration for the mouseenter event
+						animMouseenter		: {
+							'mText' : {speed : 350, easing : 'easeOutExpo', delay : 140, dir : 1},
+							'sText' : {speed : 350, easing : 'easeOutExpo', delay : 0, dir : 1},
+							'icon'  : {speed : 350, easing : 'easeOutExpo', delay : 280, dir : 1}
+						},
+						// configuration for the mouseleave event
+						animMouseleave		: {
+							'mText' : {speed : 300, easing : 'easeInExpo', delay : 140, dir : 1},
+							'sText' : {speed : 300, easing : 'easeInExpo', delay : 280, dir : 1},
+							'icon'  : {speed : 300, easing : 'easeInExpo', delay : 0, dir : 1}
+						},
+						// speed for the item bg color animation
+						boxAnimSpeed		: 300,
+						// default text color (same defined in the css)
+						defaultTextColor	: '#000',
+						// default bg color (same defined in the css)
+						defaultBgColor		: '#fff'
+					};
+					
+					return this.each(function() {
+						
+						// if options exist, lets merge them with our default settings
+						if ( options ) {
+							$.extend( settings, options );
+						}
+						
+						var $el 			= $(this),
+							// the menu items
+							$menuItems		= $el.children('li'),
+							// save max delay time for mouseleave anim parameters
+						maxdelay	= Math.max( settings.animMouseleave['mText'].speed + settings.animMouseleave['mText'].delay ,
+												settings.animMouseleave['sText'].speed + settings.animMouseleave['sText'].delay ,
+												settings.animMouseleave['icon'].speed + settings.animMouseleave['icon'].delay
+											  ),
+							// timeout for the mouseenter event
+							// lets us move the mouse quickly over the items,
+							// without triggering the mouseenter event
+							t_mouseenter;
+						
+						// save default top values for the moving elements:
+						// the elements that animate inside each menu item
+						$menuItems.find('.sti-item').each(function() {
+							var $el	= $(this);
+							$el.data('deftop', $el.position().top);
+						});
+						
+						// ************** Events *************
+						// mouseenter event for each menu item
+						$menuItems.bind('mouseenter', function(e) {
+							
+							clearTimeout(t_mouseenter);
+							
+							var $item		= $(this),
+								$wrapper	= $item.children('a'),
+								wrapper_h	= $wrapper.height(),
+								// the elements that animate inside this menu item
+								$movingItems= $wrapper.find('.sti-item'),
+								// the color that the texts will have on hover
+								hovercolor	= $item.data('hovercolor');
+							
+							t_mouseenter	= setTimeout(function() {
+								// indicates the item is on hover state
+								$item.addClass('sti-current');
+								
+								$movingItems.each(function(i) {
+									var $item			= $(this),
+										item_sti_type	= $item.data('type'),
+										speed			= settings.animMouseenter[item_sti_type].speed,
+										easing			= settings.animMouseenter[item_sti_type].easing,
+										delay			= settings.animMouseenter[item_sti_type].delay,
+										dir				= settings.animMouseenter[item_sti_type].dir,
+										// if dir is 1 the item moves downwards
+										// if -1 then upwards
+										style			= {'top' : -dir * wrapper_h + 'px'};
+									
+									if( item_sti_type === 'icon' ) {
+										// this sets another bg image for the icon
+										style.backgroundPosition	= 'bottom left';
+									} else {
+										style.color					= hovercolor;
+									}
+									// we hide the icon, move it up or down, and then show it
+									$item.hide().css(style).show();
+									clearTimeout($item.data('time_anim'));
+									$item.data('time_anim',
+										setTimeout(function() {
+											// now animate each item to its default tops
+											// each item will animate with a delay specified in the options
+											$item.stop(true)
+												 .animate({top : $item.data('deftop') + 'px'}, speed, easing);
+										}, delay)
+									);
+								});
+								// animate the bg color of the item
+								$wrapper.stop(true).animate({
+									backgroundColor: settings.defaultTextColor
+								}, settings.boxAnimSpeed );
+							
+							}, 100);	
+
+						})
+						// mouseleave event for each menu item
+						.bind('mouseleave', function(e) {
+							
+							clearTimeout(t_mouseenter);
+							
+							var $item		= $(this),
+								$wrapper	= $item.children('a'),
+								wrapper_h	= $wrapper.height(),
+								$movingItems= $wrapper.find('.sti-item');
+							
+							if(!$item.hasClass('sti-current')) 
+								return false;		
+							
+							$item.removeClass('sti-current');
+							
+							$movingItems.each(function(i) {
+								var $item			= $(this),
+									item_sti_type	= $item.data('type'),
+									speed			= settings.animMouseleave[item_sti_type].speed,
+									easing			= settings.animMouseleave[item_sti_type].easing,
+									delay			= settings.animMouseleave[item_sti_type].delay,
+									dir				= settings.animMouseleave[item_sti_type].dir;
+								
+								clearTimeout($item.data('time_anim'));
+								
+								setTimeout(function() {
+									
+									$item.stop(true).animate({'top' : -dir * wrapper_h + 'px'}, speed, easing, function() {
+										
+										if( delay + speed === maxdelay ) {
+											
+											$wrapper.stop(true).animate({
+												backgroundColor: settings.defaultBgColor
+											}, settings.boxAnimSpeed );
+											
+											$movingItems.each(function(i) {
+												var $el				= $(this),
+													style			= {'top' : $el.data('deftop') + 'px'};
+												
+												if( $el.data('type') === 'icon' ) {
+													style.backgroundPosition	= 'top left';
+												} else {
+													style.color					= settings.defaultTextColor;
+												}
+												
+												$el.hide().css(style).show();
+											});
+											
+										}
+									});
+								}, delay);
+							});
+						});
+						
+					});
+				}
+			}
+		};
+	
+	$.fn.iconmenu = function(method) {
+		if ( methods[method] ) {
+			return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( typeof method === 'object' || ! method ) {
+			return methods.init.apply( this, arguments );
+		} else {
+			$.error( 'Method ' +  method + ' does not exist on jQuery.iconmenu' );
+		}
+	};
+	
+})(jQuery);
+/*=====2======*/
+jQuery.easing['jswing'] = jQuery.easing['swing'];
+
+jQuery.extend( jQuery.easing,
+{
+	def: 'easeOutQuad',
+	swing: function (x, t, b, c, d) {
+		//alert(jQuery.easing.default);
+		return jQuery.easing[jQuery.easing.def](x, t, b, c, d);
+	},
+	easeInQuad: function (x, t, b, c, d) {
+		return c*(t/=d)*t + b;
+	},
+	easeOutQuad: function (x, t, b, c, d) {
+		return -c *(t/=d)*(t-2) + b;
+	},
+	easeInOutQuad: function (x, t, b, c, d) {
+		if ((t/=d/2) < 1) return c/2*t*t + b;
+		return -c/2 * ((--t)*(t-2) - 1) + b;
+	},
+	easeInCubic: function (x, t, b, c, d) {
+		return c*(t/=d)*t*t + b;
+	},
+	easeOutCubic: function (x, t, b, c, d) {
+		return c*((t=t/d-1)*t*t + 1) + b;
+	},
+	easeInOutCubic: function (x, t, b, c, d) {
+		if ((t/=d/2) < 1) return c/2*t*t*t + b;
+		return c/2*((t-=2)*t*t + 2) + b;
+	},
+	easeInQuart: function (x, t, b, c, d) {
+		return c*(t/=d)*t*t*t + b;
+	},
+	easeOutQuart: function (x, t, b, c, d) {
+		return -c * ((t=t/d-1)*t*t*t - 1) + b;
+	},
+	easeInOutQuart: function (x, t, b, c, d) {
+		if ((t/=d/2) < 1) return c/2*t*t*t*t + b;
+		return -c/2 * ((t-=2)*t*t*t - 2) + b;
+	},
+	easeInQuint: function (x, t, b, c, d) {
+		return c*(t/=d)*t*t*t*t + b;
+	},
+	easeOutQuint: function (x, t, b, c, d) {
+		return c*((t=t/d-1)*t*t*t*t + 1) + b;
+	},
+	easeInOutQuint: function (x, t, b, c, d) {
+		if ((t/=d/2) < 1) return c/2*t*t*t*t*t + b;
+		return c/2*((t-=2)*t*t*t*t + 2) + b;
+	},
+	easeInSine: function (x, t, b, c, d) {
+		return -c * Math.cos(t/d * (Math.PI/2)) + c + b;
+	},
+	easeOutSine: function (x, t, b, c, d) {
+		return c * Math.sin(t/d * (Math.PI/2)) + b;
+	},
+	easeInOutSine: function (x, t, b, c, d) {
+		return -c/2 * (Math.cos(Math.PI*t/d) - 1) + b;
+	},
+	easeInExpo: function (x, t, b, c, d) {
+		return (t==0) ? b : c * Math.pow(2, 10 * (t/d - 1)) + b;
+	},
+	easeOutExpo: function (x, t, b, c, d) {
+		return (t==d) ? b+c : c * (-Math.pow(2, -10 * t/d) + 1) + b;
+	},
+	easeInOutExpo: function (x, t, b, c, d) {
+		if (t==0) return b;
+		if (t==d) return b+c;
+		if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
+		return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
+	},
+	easeInCirc: function (x, t, b, c, d) {
+		return -c * (Math.sqrt(1 - (t/=d)*t) - 1) + b;
+	},
+	easeOutCirc: function (x, t, b, c, d) {
+		return c * Math.sqrt(1 - (t=t/d-1)*t) + b;
+	},
+	easeInOutCirc: function (x, t, b, c, d) {
+		if ((t/=d/2) < 1) return -c/2 * (Math.sqrt(1 - t*t) - 1) + b;
+		return c/2 * (Math.sqrt(1 - (t-=2)*t) + 1) + b;
+	},
+	easeInElastic: function (x, t, b, c, d) {
+		var s=1.70158;var p=0;var a=c;
+		if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+		if (a < Math.abs(c)) { a=c; var s=p/4; }
+		else var s = p/(2*Math.PI) * Math.asin (c/a);
+		return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
+	},
+	easeOutElastic: function (x, t, b, c, d) {
+		var s=1.70158;var p=0;var a=c;
+		if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+		if (a < Math.abs(c)) { a=c; var s=p/4; }
+		else var s = p/(2*Math.PI) * Math.asin (c/a);
+		return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;
+	},
+	easeInOutElastic: function (x, t, b, c, d) {
+		var s=1.70158;var p=0;var a=c;
+		if (t==0) return b;  if ((t/=d/2)==2) return b+c;  if (!p) p=d*(.3*1.5);
+		if (a < Math.abs(c)) { a=c; var s=p/4; }
+		else var s = p/(2*Math.PI) * Math.asin (c/a);
+		if (t < 1) return -.5*(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
+		return a*Math.pow(2,-10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )*.5 + c + b;
+	},
+	easeInBack: function (x, t, b, c, d, s) {
+		if (s == undefined) s = 1.70158;
+		return c*(t/=d)*t*((s+1)*t - s) + b;
+	},
+	easeOutBack: function (x, t, b, c, d, s) {
+		if (s == undefined) s = 1.70158;
+		return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b;
+	},
+	easeInOutBack: function (x, t, b, c, d, s) {
+		if (s == undefined) s = 1.70158; 
+		if ((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b;
+		return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b;
+	},
+	easeInBounce: function (x, t, b, c, d) {
+		return c - jQuery.easing.easeOutBounce (x, d-t, 0, c, d) + b;
+	},
+	easeOutBounce: function (x, t, b, c, d) {
+		if ((t/=d) < (1/2.75)) {
+			return c*(7.5625*t*t) + b;
+		} else if (t < (2/2.75)) {
+			return c*(7.5625*(t-=(1.5/2.75))*t + .75) + b;
+		} else if (t < (2.5/2.75)) {
+			return c*(7.5625*(t-=(2.25/2.75))*t + .9375) + b;
+		} else {
+			return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b;
+		}
+	},
+	easeInOutBounce: function (x, t, b, c, d) {
+		if (t < d/2) return jQuery.easing.easeInBounce (x, t*2, 0, c, d) * .5 + b;
+		return jQuery.easing.easeOutBounce (x, t*2-d, 0, c, d) * .5 + c*.5 + b;
+	}
+});
+
+/*animated row end*/
+
+/***************** Waypoints ******************/
+
+$(document).ready(function() {
+
+	$('.wp1').waypoint(function() {
+		$('.wp1').addClass('animated fadeInLeft');
+	}, {
+		offset: '75%'
+	});
+	$('.wp2').waypoint(function() {
+		$('.wp2').addClass('animated fadeInUp');
+	}, {
+		offset: '75%'
+	});
+	$('.wp3').waypoint(function() {
+		$('.wp3').addClass('animated fadeInDown');
+	}, {
+		offset: '55%'
+	});
+	$('.wp4').waypoint(function() {
+		$('.wp4').addClass('animated fadeInDown');
+	}, {
+		offset: '75%'
+	});
+	$('.wp5').waypoint(function() {
+		$('.wp5').addClass('animated fadeInUp');
+	}, {
+		offset: '75%'
+	});
+	$('.wp6').waypoint(function() {
+		$('.wp6').addClass('animated fadeInDown');
+	}, {
+		offset: '75%'
+	});
+
+});
+
+/***************** Slide-In Nav ******************/
+
+$(window).load(function() {
+
+	$('.nav_slide_button').click(function() {
+		$('.pull').slideToggle();
+	});
+
+});
+
+/***************** Smooth Scrolling ******************/
+
+$(function() {
+
+	$('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 2000);
+				return false;
+			}
+		}
+	});
+
+});
+
+/***************** Nav Transformicon ******************/
+
+document.querySelector("#nav-toggle").addEventListener("click", function() {
+	this.classList.toggle("active");
+});
+/***************** Overlays ******************/
+
+$(document).ready(function(){
+    if (Modernizr.touch) {
+        // show the close overlay button
+        $(".close-overlay").removeClass("hidden");
+        // handle the adding of hover class when clicked
+        $(".img").click(function(e){
+            if (!$(this).hasClass("hover")) {
+                $(this).addClass("hover");
+            }
+        });
+        // handle the closing of the overlay
+        $(".close-overlay").click(function(e){
+            e.preventDefault();
+            e.stopPropagation();
+            if ($(this).closest(".img").hasClass("hover")) {
+                $(this).closest(".img").removeClass("hover");
+            }
+        });
+    } else {
+        // handle the mouseenter functionality
+        $(".img").mouseenter(function(){
+            $(this).addClass("hover");
+        })
+        // handle the mouseleave functionality
+        .mouseleave(function(){
+            $(this).removeClass("hover");
+        });
+    }
+});
+
+/***************** Flexsliders ******************/
+
+$(window).load(function() {
+
+	$('#portfolioSlider').flexslider({
+		animation: "slide",
+		directionNav: false,
+		controlNav: true,
+		touch: false,
+		pauseOnHover: true,
+		start: function() {
+			$.waypoints('refresh');
+		}
+	});
+
+	$('#servicesSlider').flexslider({
+		animation: "slide",
+		directionNav: false,
+		controlNav: true,
+		touch: true,
+		pauseOnHover: true,
+		start: function() {
+			$.waypoints('refresh');
+		}
+	});
+
+	$('#teamSlider').flexslider({
+		animation: "slide",
+		directionNav: false,
+		controlNav: true,
+		touch: true,
+		pauseOnHover: true,
+		start: function() {
+			$.waypoints('refresh');
+		}
+	});
+
+});
+/*form*/
